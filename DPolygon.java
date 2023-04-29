f6b6847bc392bd3d56060410fcb245b806635617
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-04-29 13:48:37 -0400

    Update DPolygon.java

--- DPolygon.java
+++ DPolygon.java
@@ -1,12 +1,14 @@
 import java.awt.Color;
 
 public class DPolygon {
-    Color c;
-    double[] x, y, z;
-    boolean draw = true, seeThrough = false;
-    double[] CalcPos, newX, newY;
-    PolygonObject DrawablePolygon;
-    double AvgDist;
+    private PolygonObject DrawablePolygon;
+    private Color c;
+    
+    private double[] calcPos, newX, newY;
+    private double[] x, y, z;
+    private double avgDist;
+    
+    private boolean draw = true, seeThrough = false;
     
     public DPolygon(double[] x, double[] y,  double[] z, Color c, boolean seeThrough)
     {
@@ -30,42 +32,63 @@ public class DPolygon {
         draw = true;
         for(int i=0; i<x.length; i++)
         {
-            CalcPos = Calculator.CalculatePositionP(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
-            newX[i] = (DDDTutorial.ScreenSize.getWidth()/2 - Calculator.CalcFocusPos[0]) + CalcPos[0] * Screen.zoom;
-            newY[i] = (DDDTutorial.ScreenSize.getHeight()/2 - Calculator.CalcFocusPos[1]) + CalcPos[1] * Screen.zoom;            
-            if(Calculator.t < 0)
+            calcPos = Calculator.calculatePositionP(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
+            newX[i] = (DDDTutorial.ScreenSize.getWidth()/2 - Calculator.calcFocusPos[0]) + calcPos[0] * Screen.zoom;
+            newY[i] = (DDDTutorial.ScreenSize.getHeight()/2 - Calculator.calcFocusPos[1]) + calcPos[1] * Screen.zoom;            
+            if(Calculator.t < 0) {
                 draw = false;
+            }
         }
         
         calcLighting();
         
-        DrawablePolygon.draw = draw;
+        DrawablePolygon.setDraw(draw);
         DrawablePolygon.updatePolygon(newX, newY);
-        AvgDist = GetDist();
+        avgDist = GetDist();
     }
     
     void calcLighting()
     {
         Plane lightingPlane = new Plane(this);
-        double angle = Math.acos(((lightingPlane.ret.x * Screen.LightDir[0]) + 
-              (lightingPlane.ret.y * Screen.LightDir[1]) + (lightingPlane.ret.z * Screen.LightDir[2]))
+        double angle = Math.acos(((lightingPlane.getRetVector().getX() * Screen.LightDir[0]) + 
+              (lightingPlane.getRetVector().getY() * Screen.LightDir[1]) + (lightingPlane.getRetVector().getZ() * Screen.LightDir[2]))
               /(Math.sqrt(Screen.LightDir[0] * Screen.LightDir[0] + Screen.LightDir[1] * Screen.LightDir[1] + Screen.LightDir[2] * Screen.LightDir[2])));
         
-        DrawablePolygon.lighting = 0.2 + 1 - Math.sqrt(Math.toDegrees(angle)/180);
+        DrawablePolygon.setLighting(Calculator.clamp(0.2 + 1 - Math.sqrt(Math.toDegrees(angle)/180),0,1));
 
-        if(DrawablePolygon.lighting > 1)
-            DrawablePolygon.lighting = 1;
-        if(DrawablePolygon.lighting < 0)
-            DrawablePolygon.lighting = 0;
     }
         
     double GetDist()
     {
         double total = 0;
-        for(int i=0; i<x.length; i++)
+        for(int i=0; i<x.length; i++) {
             total += GetDistanceToP(i);
+        }
         return total / x.length;
     }
+    
+    PolygonObject getDrawablePolygon() {
+        return DrawablePolygon;
+    }
+    
+    void setDraw(boolean value) {
+        draw = value;
+    }
+    
+    boolean getDraw() {
+        return draw;
+    }
+    
+    double getAvgDist() {
+        return avgDist;
+    }
+    
+    double[] getX() { return x; }
+    double[] getY() { return y; }
+    double[] getZ() { return z; }
+    void setX(double[] value) { x = value; }
+    void setY(double[] value) { y = value; }
+    void setZ(double[] value) { z = value; } 
     
     double GetDistanceToP(int i)
     {
