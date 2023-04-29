fb95906a73de69f21f9fc077cc3266c4c89ac2a0
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-04-29 13:49:27 -0400

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,16 +1,23 @@
 
 public class Vector {
-    double x, y, z;
+    private double x, y, z;
     public Vector(double x, double y, double z) {
         double l = Math.sqrt(x*x + y*y + z*z);
-        if(l>0) {
+        if(l > 0) {
             this.x = x/l;
             this.y = y/l;
             this.z = z/l;
         }
     }
     
-    Vector CrossProduct(Vector v) {
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getZ() { return z; }
+    public void setX(double val) { x = val; }
+    public void setY(double val) { y = val; }
+    public void setZ(double val) { z = val; }
+    
+    public Vector CrossProduct(Vector v) {
         return new Vector(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
     }
 }
