d5360679f0885aa9bb7a59718c5f70c012986498
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-04-29 13:48:24 -0400

    Update DDDTutorial.java

--- DDDTutorial.java
+++ DDDTutorial.java
@@ -4,18 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class DDDTutorial 
-{	
-	static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	static JTextField TF;
-	
-	public static void main(String[] args)
-	{
-		JFrame F = new JFrame();
-		Screen ScreenObject = new Screen();
-		F.add(ScreenObject);
-		F.setUndecorated(true);
-	    F.setSize(ScreenSize);
-		F.setVisible(true);
-		F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+{    
+    static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    static JTextField TF;
+    
+    public static void main(String[] args)
+    {
+        JFrame F = new JFrame();
+        Screen ScreenObject = new Screen();
+        F.add(ScreenObject);
+        F.setUndecorated(true);
+        F.setSize(ScreenSize);
+        F.setVisible(true);
+        F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
