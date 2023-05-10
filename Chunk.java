a406f45f8d3b1d31db7afbad8638ea5f7ad16bd7
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-05-10 12:49:40 -0400

    Update Chunk.java

--- Chunk.java
+++ Chunk.java
@@ -3,23 +3,62 @@ import java.util.ArrayList;
 public class Chunk {
     private int size;
     private int height;
-    private Cube[][][] cubes;
-    public Chunk(int size, int height) {
+    private int[][][] cubeTypes;
+    private ArrayList<Cube> cubes;
+    private int x, y;
+    private boolean inMap;
+    public Chunk(int x, int y, int size, int height) {
+        this.x = x;
+        this.y = y;
         this.size = size;
         this.height = height;
-        this.cubes = new Cube[size][size][height];
+        this.cubeTypes = new int[size][size][height];
         Screen.Chunks.add(this);
     }
     
-    public void setCube(int x, int y, int z, Cube cube) {
-        cubes[x][y][z] = cube;
-    }
-
-    public Cube getCube(int x, int y, int z) {
-        return cubes[x][y][z];
+    public Chunk(int[][][] array, int size, int height, int startX, int startY) {
+        this.cubeTypes = new int[size][size][height];
+        this.size = size;
+        this.x = startX;
+        this.y = startY;
+        cubes = new ArrayList<Cube>();
+        for(int x = 0; x < size; x ++) {
+            for(int y = 0; y < size; y ++) {
+                for(int z = 0; z < height; z ++) {
+                    cubeTypes[x][y][z] = array[startX * size + x][startY * size + y][z];
+                    if(cubeTypes[x][y][z] != -1) {
+                        cubes.add(new Cube(startX * size + x, startY * size + y, z, 1, 1, 1, cubeTypes[x][y][z]));
+                    }
+                }
+            }
+        }
     }
     
-    public Cube[][][] getCubeArray() {
+    public void setCube(int x, int y, int z, int cubeType) {
+        cubeTypes[x][y][z] = cubeType;
+    }
+    
+    public boolean isAlreadyInMap() {
+        return inMap;
+    }
+    
+    public void setAlreadyInMap(boolean state) {
+        inMap = state;
+    }
+    
+    public double getDist(double x, double y) {
+        return(Math.sqrt(Math.pow(x - this.x,2) + Math.pow(y - this.y,2)));
+    }
+    
+    public int[][][] getChunk() {
+        return cubeTypes;
+    }
+    
+    public ArrayList<Cube> getCubeArray() {
         return cubes;
     }
+    
+    public int getX() {return x;}
+    public int getY() {return y;}
+    public int getSize() {return size;}
 }
